1a6ad015f6e82a7334ebbbd6aaabd93529cdc9c9
Ahmed Naguib <anagib@ntgclarity.com>
2023-05-07 22:04:59 +0300

    Add comment

--- CarsPlayer/Application/AppDelegate.swift
+++ CarsPlayer/Application/AppDelegate.swift
@@ -22,6 +22,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
           Crashes.self
         ])
         setupRootViewController()
+        // Add new comment 
         return true
     }
     
